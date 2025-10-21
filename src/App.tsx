ab97a2714d6f57337c7f271c563dee4f03291312
AFIFIHishame-Z <hishame.afifi1997@gmail.com>
2025-10-21 20:42:45 +0100

    fix build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -16,9 +13,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
