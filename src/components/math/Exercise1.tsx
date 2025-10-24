84901420064202944803ea5b3d58bd5981a7ff94
AFIFIHishame-Z <hishame.afifi1997@gmail.com>
2025-10-24 18:27:08 +0100

    Fix typo in top style property for icon positioning in Exercise1 component.

--- src/components/math/Exercise1.tsx
+++ src/components/math/Exercise1.tsx
@@ -247,7 +247,7 @@ const Exercise1 = () => {
       {/* Icon in right middle */}
       <div
         className="absolute  z-10 flex flex-col gap-3"
-        style={{ right: "35px", top: "32%%" }}
+        style={{ right: "35px", top: "32%" }}
       >
         <img
           src="/media/icons/Layer_3.png"
