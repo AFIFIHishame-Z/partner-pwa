3d715e5828ae65686b4d0e1c23f80158312c8062
AFIFIHishame-Z <hishame.afifi1997@gmail.com>
2025-10-24 13:06:11 +0100

    Refactor imports in AdditionGame and Quiz components to remove React import

--- src/components/AdditionGame.tsx
+++ src/components/AdditionGame.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function AdditionGame() {
   const [result, setResult] = useState("");
--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 
 /**
  * Pixel-accurate UI inspired by the provided mock.
