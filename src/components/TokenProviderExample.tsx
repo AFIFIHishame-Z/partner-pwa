d8699c6ad667666a02d014f79fa3a0c9829e5a9d
AFIFIHishame-Z <hishame.afifi1997@gmail.com>
2026-01-10 14:56:22 +0100

    test

--- src/components/TokenProviderExample.tsx
+++ src/components/TokenProviderExample.tsx
@@ -101,7 +101,7 @@ export function TokenProviderExample() {
         }}
       >
         <div style={{ marginBottom: "10px" }}>
-          <strong>State:</strong>{" "}
+          <strong>Stateee:</strong>{" "}
           <span
             style={{
               padding: "4px 8px",
